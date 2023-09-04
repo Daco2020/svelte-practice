6c60148bd4995946047caea2a7d6a4fb9c478a58
Daco2020 <dacokim32@gmail.com>
2023-09-04 23:19:37 +0900

    Svelte Components Basic Structure

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let name = 'Svelte';
+	let src =
+		'https://i.namu.wiki/i/U8nWyvc76Ps3GJsJGKBvDzxh1HWD0WSgncBDKNvHskPmGS5rOjxBy864oRb2gEtGeRWolxY6p99R6IEaShbSqQ.webp';
+	let string = 'Some <strong>string</strong> with html';
+</script>
+
+<img {src} alt="Sume text {name}" />
+<h1>Hello {name.toUpperCase()}!</h1>
+<p>{@html string}</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
