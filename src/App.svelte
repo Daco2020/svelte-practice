c05cd8de8ce70d5975cf5ff17b4088a43d34ff17
Daco2020 <dacokim32@gmail.com>
2023-09-06 00:36:07 +0900

    Reactive Statements

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,5 @@
 <script>
-	let name = 'Svelte';
-	let src =
-		'https://i.namu.wiki/i/U8nWyvc76Ps3GJsJGKBvDzxh1HWD0WSgncBDKNvHskPmGS5rOjxBy864oRb2gEtGeRWolxY6p99R6IEaShbSqQ.webp';
-	let string = 'Some <strong>string</strong> with html';
+	import Counter from './lib/Counter.svelte';
 </script>
 
-<img {src} alt="Sume text {name}" />
-<h1>Hello {name.toUpperCase()}!</h1>
-<p>{@html string}</p>
-
-<style>
-	h1 {
-		color: red;
-	}
-</style>
+<Counter />
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,28 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
-  }
+	let count = 0;
+	$: string = `You have clicked ${count} times`;
+
+	$: if (count >= 10) {
+		alert(`count is dangerously high!`);
+		count = 9;
+	}
+
+	function increment() {
+		count += 1;
+	}
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<button on:click={increment}>clicks {count}</button>
+<p>{string}</p>
+
+<style>
+	button {
+		font-size: 1em;
+		padding: 0.5em 1em;
+		color: #fff;
+		background-color: #ff3e00;
+		border: none;
+		border-radius: 3px;
+		cursor: pointer;
+	}
+</style>
