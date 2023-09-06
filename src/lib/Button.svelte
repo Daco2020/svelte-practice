d45ce1ca96688205e0e529569e716ad1548a6c0e
Daco2020 <dacokim32@gmail.com>
2023-09-06 21:25:43 +0900

    Adding Classes Dynamically Using The Class Directive

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,19 +1,33 @@
 <script>
+	export let size = 'small';
+	export let shadow = false;
 </script>
 
-<button><slot /></button>
-<button> <slot>Default Button</slot></button>
+<button class:size-lg={size === 'large'} class:size-sm={size === 'small'} class:shadow>
+	<slot>Default Button</slot>
+</button>
 
 <style lang="scss">
 	button {
 		background-color: #4caf50;
 		border: none;
 		color: white;
-		padding: 16px 16px;
-		font-size: 16px;
 		cursor: pointer;
 		&:hover {
-			background-color: variables.$color;
+			background-image: linear-gradient(rgba(0, 0, 0, 0.1) 0 0);
+		}
+		&:active {
+			background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
+		}
+		&.size-sm {
+			padding: 15px 20px;
+		}
+		&.size-lg {
+			padding: 20px 25px;
+			font-size: 20px;
+		}
+		&.shadow {
+			box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.3);
 		}
 	}
 </style>
